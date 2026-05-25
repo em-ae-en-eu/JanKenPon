682dc5e235c68fbdbb99889aab71a1fdfce62291
manu <12345678+username@users.noreply.github.com>
2026-05-25 21:39:10 +0530

    Complete JanKenPon Game

---
+++ script.js
@@ -0,0 +1,62 @@
+console.log('hello world');
+
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+
+  let randomNum  = Math.random();
+
+  if(randomNum <= 0.33){
+    return 'rock';
+  }else if(randomNum <= 0.66){
+    return 'paper';
+  }else{
+    return 'scissors';
+  }
+
+}
+
+function getHumanChoice(){
+    let input = prompt('Enter your Input: rock or paper or scissors');
+    return input.toLowerCase();
+    
+}
+
+function playRound(humanInput , computerInput){
+    if(humanInput === computerInput){
+        return 'Tie';
+    }else if(humanInput === "rock" && computerInput === "scissors"){
+        humanScore ++;
+        return `${humanInput} beats ${computerInput} !`;
+    }else if(humanInput === "paper" && computerInput === "rock"){
+        humanScore ++;
+        return `${humanInput} beats ${computerInput} !`;
+        
+    }else if(humanInput === "scissors" && computerInput === "paper"){
+        humanScore ++;
+        return `${humanInput} beats ${computerInput} !`;
+    }else{
+        computerScore ++;
+        return `${computerInput} beats ${humanInput}!`;      
+    }
+
+}
+
+function playGame(){
+    for(let a = 1; a<=5 ; a++){
+        console.log(playRound(getHumanChoice(), getComputerChoice()));
+    }
+
+    if(humanScore === computerScore){
+        return "THE GAME IS A TIE";
+    }else if(humanScore > computerScore){
+        return "HUMAN WINS THE GAME";
+    }else{
+        return "COMPUTER WINS THE GAME";
+    }
+}
+
+console.log(playGame());
